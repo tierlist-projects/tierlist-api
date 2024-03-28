14e20ea40facc97839ea383bec732d03441582e7
김덕윤 <duk9741@gmail.com>
2024-03-28 23:08:41 +0900

    chore: init project

---
+++ src/main/java/com/tierlist/tierlist/TierlistApplication.java
@@ -0,0 +1,13 @@
+package com.tierlist.tierlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tierlist/tierlist/TierlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tierlist.tierlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
