d8be77c5f06284bc5d3fa137a69cdea871226c28
김덕윤 <duk9741@gmail.com>
2024-04-16 16:17:26 +0900

    feat: nickname validator 추가

---
+++ src/main/java/com/tierlist/tierlist/member/application/domain/validation/Nickname.java
@@ -0,0 +1,25 @@
+package com.tierlist.tierlist.member.application.domain.validation;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import com.tierlist.tierlist.member.application.domain.validation.validator.NicknameValidator;
+import jakarta.validation.Constraint;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({FIELD})
+@Retention(RUNTIME)
+@Constraint(validatedBy = NicknameValidator.class)
+@Documented
+public @interface Nickname {
+
+  String message() default "nickname은 2자 이상 10자 이하, 영어, 숫자 또는 한글로 구성되어야 하고,"
+      + "공백, 특수문자, 자음, 모음을 포함할 수 없습니다.";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends String>[] payload() default {};
+
+}
---
+++ src/main/java/com/tierlist/tierlist/member/application/domain/validation/validator/NicknameValidator.java
@@ -0,0 +1,16 @@
+package com.tierlist.tierlist.member.application.domain.validation.validator;
+
+import com.tierlist.tierlist.member.application.domain.validation.Nickname;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.Objects;
+
+public class NicknameValidator implements ConstraintValidator<Nickname, String> {
+
+  private static final String REGEX = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,10}$";
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    return !Objects.isNull(value) && value.matches(REGEX);
+  }
+}
