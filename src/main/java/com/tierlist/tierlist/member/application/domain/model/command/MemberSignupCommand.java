f0169c878c94f1bec9e934734d7c5c5d96d4b5b1
김덕윤 <duk9741@gmail.com>
2024-04-16 16:36:57 +0900

    feat: MemberSignupCommand validator 적용

--- src/main/java/com/tierlist/tierlist/member/application/domain/model/command/MemberSignupCommand.java
+++ src/main/java/com/tierlist/tierlist/member/application/domain/model/command/MemberSignupCommand.java
@@ -1,26 +1,40 @@
 package com.tierlist.tierlist.member.application.domain.model.command;
 
-import com.tierlist.tierlist.member.application.domain.model.EmailVerificationCode;
+import com.tierlist.tierlist.global.validation.Validator;
 import com.tierlist.tierlist.member.application.domain.model.Member;
 import com.tierlist.tierlist.member.application.domain.model.Password;
-import lombok.AllArgsConstructor;
+import com.tierlist.tierlist.member.application.domain.validation.EmailVerificationCode;
+import com.tierlist.tierlist.member.application.domain.validation.Nickname;
+import jakarta.validation.constraints.Email;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Builder
-@AllArgsConstructor
 @Getter
 public class MemberSignupCommand {
 
-
-  private String nickname;
-
-  private String rawPassword;
-
+  @Email
   private String email;
 
-  private EmailVerificationCode code;
+  @Nickname
+  private String nickname;
+
+  @com.tierlist.tierlist.member.application.domain.validation.Password
+  private String rawPassword;
+
+  @EmailVerificationCode
+  private String code;
+
+  public MemberSignupCommand(String email, String nickname, String rawPassword,
+      String code) {
+    this.email = email;
+    this.nickname = nickname;
+    this.rawPassword = rawPassword;
+    this.code = code;
+
+    Validator.validate(this);
+  }
 
   public Member toMember(PasswordEncoder passwordEncoder) {
     return Member.builder()
