c308cc8a0c74da2e996837f1e79e662ee9e60b59
김덕윤 <duk9741@gmail.com>
2024-04-18 17:42:02 +0900

    refactor: RandomEmailVerificationCodeGenerator Math.random() 대신 Random.nextInt()로 변경

--- src/main/java/com/tierlist/tierlist/member/adapter/out/infrastructure/RandomEmailVerificationCodeGenerator.java
+++ src/main/java/com/tierlist/tierlist/member/adapter/out/infrastructure/RandomEmailVerificationCodeGenerator.java
@@ -2,14 +2,18 @@ package com.tierlist.tierlist.member.adapter.out.infrastructure;
 
 import com.tierlist.tierlist.member.application.domain.model.EmailVerificationCode;
 import com.tierlist.tierlist.member.application.port.out.infrastructure.EmailVerificationCodeGenerator;
+import java.util.Random;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomEmailVerificationCodeGenerator implements EmailVerificationCodeGenerator {
 
+  private static final int MAX_VALUE = 999_999;
+  private static Random random = new Random();
+
   @Override
   public EmailVerificationCode generate() {
-    int number = (int) (Math.random() * 1000000);
+    int number = random.nextInt(MAX_VALUE + 1);
     String code = String.format("%06d", number);
 
     return EmailVerificationCode.of(code);
