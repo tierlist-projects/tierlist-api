a218e1686266be235c389ecb4eb403a01bf253bb
김덕윤 <duk9741@gmail.com>
2024-04-20 12:51:25 +0900

    feat: 이미지 업로드 관련 DTO 및 인페이스 추가

---
+++ src/main/java/com/tierlist/tierlist/image/adapter/in/web/dto/response/ImagesResponse.java
@@ -0,0 +1,18 @@
+package com.tierlist.tierlist.image.adapter.in.web.dto.response;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class ImagesResponse {
+
+  private List<String> imageNames;
+
+}
