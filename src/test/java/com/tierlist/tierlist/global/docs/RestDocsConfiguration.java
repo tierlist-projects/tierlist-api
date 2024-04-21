71fce9dfce10729941546b61e33e596ee8c3b8e4
김덕윤 <duk9741@gmail.com>
2024-04-21 23:35:59 +0900

    docs: HTTP 응답 ARRAY 일 시 줄바꿈 적용

--- src/test/java/com/tierlist/tierlist/global/docs/RestDocsConfiguration.java
+++ src/test/java/com/tierlist/tierlist/global/docs/RestDocsConfiguration.java
@@ -1,14 +1,22 @@
 package com.tierlist.tierlist.global.docs;
 
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
+import com.fasterxml.jackson.core.PrettyPrinter;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.ContentModifyingOperationPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationPreprocessor;
 
 @TestConfiguration
 public class RestDocsConfiguration {
@@ -18,6 +26,10 @@ public class RestDocsConfiguration {
     return MockMvcRestDocumentation.document(
         "{class-name}/{method-name}",  // 문서 이름 설정
         preprocessRequest(  // 공통 헤더 설정
+            modifyUris()
+                .scheme("https")
+                .host("api.tierlist.site")
+                .removePort(),
             modifyHeaders()
                 .remove("Content-Length")
                 .remove("Host"),
@@ -33,7 +45,22 @@ public class RestDocsConfiguration {
                 .remove("Expires")
                 .remove("X-Frame-Options")
                 .remove("Vary"),
-            prettyPrint())    // pretty json 적용
+            customPrettyPrint())    // pretty json 적용
     );
+  }
+
+  private OperationPreprocessor customPrettyPrint() {
+    return new ContentModifyingOperationPreprocessor(
+        (content, contentType) -> {
+          ObjectMapper objectMapper = new ObjectMapper();
+          PrettyPrinter prettyPrinter = new DefaultPrettyPrinter()
+              .withArrayIndenter(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
+          try {
+            return objectMapper.writer(prettyPrinter)
+                .writeValueAsBytes(objectMapper.readTree(content));
+          } catch (IOException ex) {
+            return content;
+          }
+        });
   }
 }
