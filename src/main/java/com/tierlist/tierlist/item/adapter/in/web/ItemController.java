cb2ba07667ab0bdfda68473f7b0aa8ef5683c2d2
김덕윤 <duk9741@gmail.com>
2024-04-23 02:52:48 +0900

    refactor: ItemCreateController ItemController로 변경

--- src/main/java/com/tierlist/tierlist/item/adapter/in/web/ItemController.java
+++ src/main/java/com/tierlist/tierlist/item/adapter/in/web/ItemController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-public class ItemCreateController {
+public class ItemController {
 
   private final ItemCreateUseCase itemCreateUseCase;
 
