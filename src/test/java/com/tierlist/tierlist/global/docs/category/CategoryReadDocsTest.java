0df4cd57f253cc6c5afc3fbbc3023bf02d348532
김덕윤 <duk9741@gmail.com>
2024-04-30 17:04:33 +0900

    docs: 카테고리 목록 조회 문서 업데이트

--- src/test/java/com/tierlist/tierlist/global/docs/category/CategoryReadDocsTest.java
+++ src/test/java/com/tierlist/tierlist/global/docs/category/CategoryReadDocsTest.java
@@ -1,7 +1,6 @@
 package com.tierlist.tierlist.global.docs.category;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
@@ -33,36 +32,45 @@ class CategoryReadDocsTest extends RestDocsTestSupport {
   @Test
   void read_category_200() throws Exception {
 
-    int pageCount = 1;
-    int pageSize = 10;
+    int page = 0;
+    int size = 3;
     String query = "카테";
     CategoryFilter filter = CategoryFilter.HOT;
 
-    given(categoryReadUseCase.getCategories(anyInt(), anyInt(), any(), any())).willReturn(
-        List.of(
-            CategoryResponse.builder()
-                .id(1L)
-                .name("카테고리1")
-                .isFavorite(false)
-                .build(),
-            CategoryResponse.builder()
-                .id(2L)
-                .name("카테고리2")
-                .isFavorite(true)
-                .build(),
-            CategoryResponse.builder()
-                .id(3L)
-                .name("카테고리3")
-                .isFavorite(false)
-                .build()
-        )
+    given(categoryReadUseCase.getCategories(any(), any(), any(), any())).willReturn(
+        PageResponse.<CategoryResponse>builder()
+            .numberOfElements(2)
+            .pageNumber(0)
+            .pageSize(2)
+            .totalElements(3)
+            .totalPages(2)
+            .content(
+                List.of(
+                    CategoryResponse.builder()
+                        .id(1L)
+                        .name("카테고리1")
+                        .isFavorite(false)
+                        .build(),
+                    CategoryResponse.builder()
+                        .id(2L)
+                        .name("카테고리2")
+                        .isFavorite(true)
+                        .build(),
+                    CategoryResponse.builder()
+                        .id(3L)
+                        .name("카테고리3")
+                        .isFavorite(false)
+                        .build()
+                )
+            )
+            .build()
     );
 
     mvc.perform(get("/category")
             .contentType(APPLICATION_JSON)
             .header("Access-Token", "sample.access.token")
-            .queryParam("pageCount", String.valueOf(pageCount))
-            .queryParam("pageSize", String.valueOf(pageSize))
+            .queryParam("page", String.valueOf(page))
+            .queryParam("size", String.valueOf(size))
             .queryParam("query", query)
             .queryParam("filter", filter.name())
         )
@@ -74,30 +82,40 @@ class CategoryReadDocsTest extends RestDocsTestSupport {
                     .optional()
             ),
             queryParameters(
-                parameterWithName("pageCount")
-                    .description("페이지 넘버")
-                    .attributes(constraints("1부터 시작")),
-                parameterWithName("pageSize")
-                    .description("페이지 당 컨텐츠 갯수")
-                    .attributes(constraints("1부터 시작")),
+                parameterWithName("page")
+                    .description("페이지 넘버, default = 0")
+                    .attributes(constraints("0부터 시작")),
+                parameterWithName("size")
+                    .description("페이지 당 컨텐츠 갯수, default = 20"),
                 parameterWithName("query")
                     .description("검색어")
                     .optional()
                     .attributes(constraints("2글자 이상")),
                 parameterWithName("filter")
-                    .description("정렬 필터 HOT: 즐겨찾기 많은 순서 NONE: 이름 오름차순")
+                    .description("정렬 필터 HOT: 즐겨찾기 많은 순서 NONE: 이름 오름차순(없을 시 NONE)")
                     .attributes(constraints("HOT, NONE 중 하나여야 함."))
+                    .optional()
             ),
             responseFields(
-                fieldWithPath("[]")
+                fieldWithPath("numberOfElements")
+                    .description("컨텐츠의 갯수"),
+                fieldWithPath("pageNumber")
+                    .description("현재 페이지 번호(0부터 시작)"),
+                fieldWithPath("pageSize")
+                    .description("페이지당 컨텐츠 갯수"),
+                fieldWithPath("totalPages")
+                    .description("전체 페이지 갯수"),
+                fieldWithPath("totalElements")
+                    .description("컨텐츠 전체의 갯수"),
+                fieldWithPath("content.[]")
                     .description("카테고리 목록"),
-                fieldWithPath("[].id")
+                fieldWithPath("content.[].id")
                     .description("카테고리 식별번호"),
-                fieldWithPath("[].name")
+                fieldWithPath("content.[].name")
                     .description("카테고리 이름"),
-                fieldWithPath("[].isFavorite")
-                    .description("즐겨찾기 여부. 로그인 안했을 시 모두 false"),
-                fieldWithPath("[].favoriteCount")
+                fieldWithPath("content.[].isFavorite")
+                    .description("카테고리 즐겨찾기 여부"),
+                fieldWithPath("content.[].favoriteCount")
                     .description("카테고리 즐겨찾기 갯수")
             )
         ));
@@ -142,6 +160,7 @@ class CategoryReadDocsTest extends RestDocsTestSupport {
             requestHeaders(
                 headerWithName("Access-Token")
                     .description("JWT Access Token")
+                    .optional()
             ),
             queryParameters(
                 parameterWithName("page")
