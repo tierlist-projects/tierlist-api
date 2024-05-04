f381acd22e592a26df7c8c1170970ad908561682
김덕윤 <duk9741@gmail.com>
2024-05-05 04:37:49 +0900

    chore: Jpa Auditing 설정 클래스로 분리

--- src/main/java/com/tierlist/tierlist/global/jpa/JpaAuditingConfig.java
+++ src/main/java/com/tierlist/tierlist/global/jpa/JpaAuditingConfig.java
@@ -1,7 +1,9 @@
 package com.tierlist.tierlist.global.jpa;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfig {
 
