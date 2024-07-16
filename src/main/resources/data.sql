a254123c3e1fefa3803dc02b0ab20aeaa1416299
김덕윤 <duk9741@gmail.com>
2024-07-17 00:34:18 +0900

    chore: 개발용 초기 회원 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO member(email, nickname, password)
 VALUES ('duk9741@gmail.com', '덕윤', '{noop}123123@!');
+INSERT INTO member(email, nickname, password)
+VALUES ('aaa@gmail.com', '감자', '{noop}123123@!');
+INSERT INTO member(email, nickname, password)
+VALUES ('bbb@gmail.com', '고구마', '{noop}123123@!');
