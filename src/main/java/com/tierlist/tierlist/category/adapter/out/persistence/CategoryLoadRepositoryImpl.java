c8c34173b959313fd6d6d7c3f619cfccb04c8efe
김덕윤 <duk9741@gmail.com>
2024-07-21 15:40:58 +0900

    fix: 카테고리 조회 즐겨찾기 여부 응답 문제 해결

--- src/main/java/com/tierlist/tierlist/category/adapter/out/persistence/CategoryLoadRepositoryImpl.java
+++ src/main/java/com/tierlist/tierlist/category/adapter/out/persistence/CategoryLoadRepositoryImpl.java
@@ -119,7 +119,7 @@ public class CategoryLoadRepositoryImpl implements CategoryLoadRepository {
                 categoryJpaEntity.name,
                 new CaseBuilder()
                     .when(memberJpaEntity.id.isNotNull()).then(1).otherwise(0)
-                    .max().gt(1).as("isFavorite"),
+                    .max().gt(0).as("isFavorite"),
                 categoryJpaEntity.favoriteCount
             ))
         .from(categoryJpaEntity)
